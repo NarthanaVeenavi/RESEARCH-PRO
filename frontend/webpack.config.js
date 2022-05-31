3dfce7d44a905160f548a59308fb74f6d3452640
Narthana Veenavi <narthanamendis1997@gmail.com>
2022-05-31 23:44:03 +0530

    Project setup

---
+++ frontend/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.js"),
+  output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
+  mode: "development",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".css", ".jsx"],
+  },
+  devServer: { port: 3000, hot: true, open: true, historyApiFallback: true },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+};
