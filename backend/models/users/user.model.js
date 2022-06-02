904e414ffcbbdef20cd3616016eeea4b38d41c05
HansakaDilshanJayawardana <hansaka345@gmail.com>
2022-06-02 22:31:58 +0530

    User Model JS File

---
+++ backend/models/users/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstname: {
+        type: String,
+        required: true,
+    },
+    lastname:{
+        type: String,
+        required: true,
+    },
+    role:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    }
+});
+
+const User = mongoose.model("user", userSchema);
+module.exports = User;
