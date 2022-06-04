384868123a296e5aa836192d66c5b35d9bf8093b
NethmaSajani <ynsajani@gmail.com>
2022-06-05 00:27:36 +0530

    Home file

---
+++ frontend/src/component/Home/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import "./Homestyle.css";
+import Footer from "./Footer";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <div className="home_div1" style={{"height": "17cm"}}>
+        <div className="home_div2">RESEARCH PROJECT MANAGEMENT TOOL</div>
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
