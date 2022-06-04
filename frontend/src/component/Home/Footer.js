ca0661450f558b0fcf388eb7d5c2e5abb0813304
NethmaSajani <ynsajani@gmail.com>
2022-06-05 00:27:22 +0530

    Header Footer Files

---
+++ frontend/src/component/Home/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer_div1">
+        <div className="footer_div2">
+          Copyright &reg; NSSH {new Date().getFullYear()}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
+
